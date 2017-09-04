9f7e871400f316f451c7b0ae4e75e30b5c8c6044
Jeff Ripke <jripke74@gmail.com>
2017-09-04 14:33:05 -0700

    add button, DOM elements

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const myHeading1 = document.getElementById('myHeading');
+const myButton1 = document.getElementById('myButton')
+const myTextInput1 = document.getElementById('myTextInput')
+
+
+
+myButton1.addEventListener('click', () => {
+  myHeading1.style.color = myTextInput.value;
+
+});
